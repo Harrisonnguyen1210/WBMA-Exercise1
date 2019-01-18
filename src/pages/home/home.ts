4d2d65e06f7ffd977984b5a8c1f7bf80900fdd77
Harrisonnguyen1210 <giahungftu2@gmail.com>
2019-01-18 14:54:05 +0200

    "Finished task 3B done"

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,23 +10,48 @@ import { Pic } from '../../interface/pic';
 })
 export class HomePage {
   // add this to HomePage component
-  constructor(public navCtrl: NavController, private photoViewer: PhotoViewer, private http: HttpClient) {
+  constructor(
+    public navCtrl: NavController, private photoViewer: PhotoViewer,
+    private http: HttpClient) {
   }
-  picArray: Pic[] = [];
-  url = "http://media.mw.metropolia.fi/wbma/uploads/";
 
-  ngOnInit(){
+  picArray: Pic[] = [];
+  myArray: Pic[] = [];
+  url = 'http://media.mw.metropolia.fi/wbma/uploads/';
+
+  ngOnInit() {
     this.fetchArray();
   }
 
   fetchArray = () => {
-    this.http.get<Pic[]>('http://media.mw.metropolia.fi/wbma/media').subscribe((res: Pic[]) => {
-      this.picArray = res;
-      console.log(res);
-    });
+    this.http.get<Pic[]>('http://media.mw.metropolia.fi/wbma/media').
+      subscribe((res: Pic[]) => {
+        this.picArray = res;
+        this.picArray.map(item => {
+          item.thumbnail = item.filename.substring(0,
+            item.filename.length - 4) + '-tn160.png';
+        });
+
+        //Another way to use map
+        // this.myArray = res.map(item => (
+        //   {
+        //     file_id: item.file_id,
+        //     filename: item.filename,
+        //     filesize: item.filesize,
+        //     user_id: item.user_id,
+        //     media_type: item.media_type,
+        //     mime_type: item.mime_type,
+        //     time_added: item.time_added,
+        //     title: item.title,
+        //     description: item.description,
+        //     thumbnail: item.filename.substring(0, item.filename.length - 4) +
+        //       '-tn160.png',
+        //   }
+        // ));
+      });
   };
 
   openPic = (imageSource) => {
     this.photoViewer.show(imageSource);
-  }
+  };
 }
